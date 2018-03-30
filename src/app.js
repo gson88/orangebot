88591d79490b02f504996b2a068a7f6ee0eb1876
gson88 <gsonmx@gmail.com>
2018-03-30 11:12:46 +0200

    First commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// const argv = require('minimist')(process.argv.slice(2));
+// if (argv.h === true) {
+//   console.log('Usage:             node orangebot.js [-i config.json] [-h]');
+//   console.log('Description:       OrangeBot v3.0 is a CS:GO matchmaking bot written in node.js.');
+//   console.log('GitHub:            https://github.com/dejavueakay/orangebot');
+//   console.log();
+//   console.log('Arguments:');
+//   console.log(' -i config.json                    Set the config.json file to use');
+//   console.log(' -h                                See this help');
+//   console.log();
+//   console.log('For further documentation, visit our GitHub wiki: https://github.com/dejavueakay/orangebot/wiki');
+//   process.exit();
+// }
+
